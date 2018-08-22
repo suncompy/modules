6ded8d06a6eed09ffb74de925eaeb9a0e62dfa8d
caiqianyi <cqy010203>
2018-08-22 23:48:39 +0800

    增加广告信息获取接口

---
+++ operate/operate-dao/src/main/java/com/lebaoxun/modules/operate/dao/AdPosDao.java
@@ -0,0 +1,17 @@
+package com.lebaoxun.modules.operate.dao;
+
+import com.lebaoxun.modules.operate.entity.AdPosEntity;
+import org.apache.ibatis.annotations.Mapper;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * 广告位
+ * 
+ * @author caiqianyi
+ * @email 270852221@qq.com
+ * @date 2018-08-22 23:06:49
+ */
+@Mapper
+public interface AdPosDao extends BaseMapper<AdPosEntity> {
+	
+}
