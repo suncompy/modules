61b02d1c554ff5b40298b14a7c46219671e9212f
caiqianyi <cqy010203>
2018-09-13 22:46:43 +0800

    修改关联产品问题

--- fastfood/fastfood-service/src/main/java/com/lebaoxun/modules/fastfood/service/impl/FoodMachineAisleServiceImpl.java
+++ fastfood/fastfood-service/src/main/java/com/lebaoxun/modules/fastfood/service/impl/FoodMachineAisleServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+import com.lebaoxun.modules.fastfood.entity.FoodMachineCatAisleEntity;
+import com.lebaoxun.modules.fastfood.service.FoodMachineCatAisleService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,10 +21,13 @@ import com.lebaoxun.modules.fastfood.entity.FoodMachineAisleEntity;
 import com.lebaoxun.modules.fastfood.entity.FoodMachineRefAisleEntity;
 import com.lebaoxun.modules.fastfood.service.FoodMachineAisleService;
 
+import javax.annotation.Resource;
+
 
 @Service("foodMachineAisleService")
 public class FoodMachineAisleServiceImpl extends ServiceImpl<FoodMachineAisleDao, FoodMachineAisleEntity> implements FoodMachineAisleService {
-
+    @Resource
+    private FoodMachineCatAisleService foodMachineCatAisleService;
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
         Page<FoodMachineAisleEntity> page = this.selectPage(
@@ -36,18 +41,38 @@ public class FoodMachineAisleServiceImpl extends ServiceImpl<FoodMachineAisleDao
     @Transactional(readOnly = false, propagation = Propagation.REQUIRES_NEW)
     public void refProductAndType(Long adminId, FoodMachineRefAisleEntity foodMachineAisle){
         List<String> _ids=foodMachineAisle.getAisleIds();
-        if (_ids==null||_ids.size()==0)
-            throw new I18nMessageException("100002", "机器渠道关联ID不能为空!");
+        List<String> catAisleIds=foodMachineAisle.getCatAisleIds();
+        if (catAisleIds==null||catAisleIds.size()==0)
+            throw new I18nMessageException("100002", "机器分类货道ID不能为空!");
         //遍历批量集，更新产品、分类id到关联表中
-        for (String id:_ids){
+        for (int i=0;i<catAisleIds.size();i++){
+            FoodMachineCatAisleEntity foodMachineCatAisleEntity=foodMachineCatAisleService.selectById(catAisleIds.get(i));
             FoodMachineAisleEntity _aisleObj=new FoodMachineAisleEntity();
-            _aisleObj.setId(Integer.parseInt(id));
             _aisleObj.setUpdateBy(adminId);
             _aisleObj.setUpdateTime(new Date());
             _aisleObj.setPrice(foodMachineAisle.getPrice());
             _aisleObj.setProductId(foodMachineAisle.getProductId());
             _aisleObj.setProductCatId(foodMachineAisle.getProductCatId());
-            updateById(_aisleObj);
+            if(_ids==null||_ids.size()==0||_ids.get(i)==null||_ids.get(i).equals("0")){
+                _aisleObj.setX(foodMachineCatAisleEntity.getX());
+                _aisleObj.setY(foodMachineCatAisleEntity.getY());
+                _aisleObj.setY(foodMachineCatAisleEntity.getY());
+                _aisleObj.setMacId(foodMachineAisle.getMacId());
+                _aisleObj.setSize(foodMachineCatAisleEntity.getSize());
+                _aisleObj.setCreateTime(new Date());
+                //一个货道只能关联一个产品
+                EntityWrapper<FoodMachineAisleEntity> entityWrapper=new EntityWrapper<>();
+                entityWrapper.eq("mac_id",foodMachineAisle.getMacId());
+                entityWrapper.eq("x",foodMachineCatAisleEntity.getX());
+                entityWrapper.eq("y",foodMachineCatAisleEntity.getY());
+                FoodMachineAisleEntity old=this.selectOne(entityWrapper);
+                if (old!=null&&old.getId()>0)
+                    continue;
+                insert(_aisleObj);
+            }else{
+                _aisleObj.setId(Integer.parseInt(_ids.get(i)));
+                updateById(_aisleObj);
+            }
         }
     }
 
