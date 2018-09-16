e28b01c0a6b99c11076c1a481102adf3bffef28c
caiqianyi <cqy010203>
2018-09-16 13:45:13 +0800

    修改测试BUG

--- fastfood/fastfood-dao/src/main/java/com/lebaoxun/modules/fastfood/dao/FoodMachineAddStockOrderDao.java
+++ fastfood/fastfood-dao/src/main/java/com/lebaoxun/modules/fastfood/dao/FoodMachineAddStockOrderDao.java
@@ -1,8 +1,13 @@
 package com.lebaoxun.modules.fastfood.dao;
 
 import com.lebaoxun.modules.fastfood.entity.FoodMachineAddStockOrderEntity;
+import com.lebaoxun.modules.fastfood.entity.FoodMachineAdvanceTimeEntity;
 import org.apache.ibatis.annotations.Mapper;
 import com.baomidou.mybatisplus.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * 取餐机进货派单
@@ -13,5 +18,11 @@ import com.baomidou.mybatisplus.mapper.BaseMapper;
  */
 @Mapper
 public interface FoodMachineAddStockOrderDao extends BaseMapper<FoodMachineAddStockOrderEntity> {
-	
+    List<Map<String,Object>> queryReplenishManList(@Param("userName")String userName,
+                                                   @Param("mobile")String mobile,
+                                                   @Param("createTime")String createTime);
+
+    List<Map<String,Object>> queryPickingManList(@Param("userName")String userName,
+                                                   @Param("mobile")String mobile,
+                                                   @Param("createTime")String createTime);
 }
--- fastfood/fastfood-dao/src/main/java/com/lebaoxun/modules/fastfood/dao/FoodMachineRepairOrderDao.java
+++ fastfood/fastfood-dao/src/main/java/com/lebaoxun/modules/fastfood/dao/FoodMachineRepairOrderDao.java
@@ -3,6 +3,10 @@ package com.lebaoxun.modules.fastfood.dao;
 import com.lebaoxun.modules.fastfood.entity.FoodMachineRepairOrderEntity;
 import org.apache.ibatis.annotations.Mapper;
 import com.baomidou.mybatisplus.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * 维修派单表
@@ -13,5 +17,7 @@ import com.baomidou.mybatisplus.mapper.BaseMapper;
  */
 @Mapper
 public interface FoodMachineRepairOrderDao extends BaseMapper<FoodMachineRepairOrderEntity> {
-	
+    List<Map<String,Object>> queryMaintenanceManList(@Param("userName")String userName,
+                                                 @Param("mobile")String mobile,
+                                                 @Param("createTime")String createTime);
 }
