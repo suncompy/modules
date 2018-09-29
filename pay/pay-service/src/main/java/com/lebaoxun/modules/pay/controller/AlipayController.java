976cf626212f07cffef3baa282e7a8b6a3f6550f
caiqianyi <270852221@qq.com>
2018-09-29 16:36:39 +0800

    1

--- pay/pay-service/src/main/java/com/lebaoxun/modules/pay/controller/AlipayController.java
+++ pay/pay-service/src/main/java/com/lebaoxun/modules/pay/controller/AlipayController.java
@@ -20,10 +20,10 @@ import com.alipay.api.AlipayApiException;
 import com.alipay.api.AlipayClient;
 import com.alipay.api.DefaultAlipayClient;
 import com.alipay.api.domain.AlipayTradeWapPayModel;
+import com.alipay.api.domain.ExtendParams;
 import com.alipay.api.internal.util.AlipaySignature;
-import com.alipay.api.request.AlipayTradeFastpayRefundQueryRequest;
 import com.alipay.api.request.AlipayTradeWapPayRequest;
-import com.alipay.api.response.AlipayTradeFastpayRefundQueryResponse;
+import com.alipay.api.response.AlipayTradePrecreateResponse;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.google.gson.Gson;
 import com.lebaoxun.commons.exception.I18nMessageException;
@@ -164,6 +164,85 @@ public class AlipayController {
 		 * SuccessMessage(alipaySubmit.buildRequest(aplipayReqest));
 		 */
 	}
+	
+	/**
+	 * 
+	 * @param group
+	 * @param outTradeNo (必填) 商户网站订单系统中唯一订单号，64个字符以内，只能包含字母、数字、下划线，
+        				 需保证商户系统端不能重复，建议通过数据库sequence生成
+	 * @param subject  (必填) 订单标题，粗略描述用户的支付目的。如“xxx品牌xxx门店当面付扫码消费”
+	 * @param totalAmount (必填) 订单总金额，单位为元，不能超过1亿元
+	 * @param undiscountableAmount (可选) 订单不可打折金额，可以配合商家平台配置折扣活动，如果酒水不参与打折，则将对应金额填写至此字段
+	 * @param rechargeFee
+	 * @param body 订单描述，可以对交易或商品进行一个详细地描述，比如填写"购买商品2件共15.00元"
+	 * @param spbill_create_ip
+	 * @param userId
+	 * @param scene
+	 */
+	/*// 测试当面付2.0生成支付二维码
+    public void test_trade_precreate(
+			@RequestParam("group") String group,
+			@RequestParam("outTradeNo") String outTradeNo,
+			@RequestParam("subject") String subject,
+			@RequestParam("totalAmount") String totalAmount,
+			@RequestParam(value="undiscountableAmount",required=false) String undiscountableAmount,
+			@RequestParam(value = "rechargeFee", required = false) BigDecimal rechargeFee,
+			@RequestParam(value = "body", required = false) String body,
+			@RequestParam(value = "spbill_create_ip", required = false) String spbill_create_ip,
+			@RequestParam(value = "userId") Long userId,
+			@RequestParam(value = "scene", required = false) String scene) {
+
+        // 卖家支付宝账号ID，用于支持一个签约账号下支持打款到不同的收款账号，(打款到sellerId对应的支付宝账号)
+        // 如果该字段为空，则默认为与支付宝签约的商户的PID，也就是appid对应的PID
+        String sellerId = "";
+
+        // 商户操作员编号，添加此参数可以为商户操作员做销售统计
+        String operatorId = "test_operator_id";
+
+        // (必填) 商户门店编号，通过门店号和商家后台可以配置精准到门店的折扣信息，详询支付宝技术支持
+        String storeId = "test_store_id";
+
+        // 业务扩展参数，目前可添加由支付宝分配的系统商编号(通过setSysServiceProviderId方法)，详情请咨询支付宝技术支持
+        ExtendParams extendParams = new ExtendParams();
+        extendParams.setSysServiceProviderId("2088100200300400500");
+
+        // 创建扫码支付请求builder，设置请求参数
+        AlipayTradePrecreateRequestBuilder builder = new AlipayTradePrecreateRequestBuilder()
+            .setSubject(subject).setTotalAmount(totalAmount).setOutTradeNo(outTradeNo)
+            .setUndiscountableAmount(undiscountableAmount).setSellerId(sellerId).setBody(body)
+            .setOperatorId(operatorId).setStoreId(storeId).setExtendParams(extendParams)
+            .setTimeoutExpress(timeoutExpress)
+            //                .setNotifyUrl("http://www.test-notify-url.com")//支付宝服务器主动通知商户服务器里指定的页面http路径,根据需要设置
+            .setGoodsDetailList(goodsDetailList);
+
+        AlipayF2FPrecreateResult result = tradeService.tradePrecreate(builder);
+        switch (result.getTradeStatus()) {
+            case SUCCESS:
+                log.info("支付宝预下单成功: )");
+
+                AlipayTradePrecreateResponse response = result.getResponse();
+                dumpResponse(response);
+
+                // 需要修改为运行机器上的路径
+                String filePath = String.format("/Users/sudo/Desktop/qr-%s.png",
+                    response.getOutTradeNo());
+                log.info("filePath:" + filePath);
+                //                ZxingUtils.getQRCodeImge(response.getQrCode(), 256, filePath);
+                break;
+
+            case FAILED:
+                log.error("支付宝预下单失败!!!");
+                break;
+
+            case UNKNOWN:
+                log.error("系统异常，预下单状态未知!!!");
+                break;
+
+            default:
+                log.error("不支持的交易状态，交易返回异常!!!");
+                break;
+        }
+    }*/
 
 	@RequestMapping(value = "/alipay/notify")
 	String notify(@RequestParam Map<String, String> params) {
